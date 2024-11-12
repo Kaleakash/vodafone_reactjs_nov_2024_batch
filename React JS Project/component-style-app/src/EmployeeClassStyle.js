5b0ce0da8ebfa55abc3512961d83229c34948d42
Akash <akash300383@gmail.com>
2024-11-12 18:07:21 +0530

    day 2 notes

--- React JS Project/component-style-app/src/EmployeeClassStyle.js
+++ React JS Project/component-style-app/src/EmployeeClassStyle.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 class EmployeeClassStyle extends React.Component{
+    constructor() {
+        super();        // call super class constructor ie Component class constructor 
+        this.state = {id:100,name:"Steven",age:21}
+    }
 
     render(){
 
         return(
             <div>
                 <h3>Class style component creation</h3>
+                <p>Employee details</p>
+                <p>Id is {this.state.id} Name is {this.state.name} Age is {this.state.age}</p>
             </div>
         )
     }
--- React JS Project/component-style-app/src/EmployeeFunctionStyle.js
+++ React JS Project/component-style-app/src/EmployeeFunctionStyle.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function EmployeeFunctionStyle() {
+let id =useState(101);
+let name = useState("John")
+let age = useState(25);
+
     return(
         <div>
             <h3>Function style component</h3>
+            <p>Employee details in function style</p>
+            <p>Id is {id} Name is {name} Age is {age}</p>
         </div>
     )
 }
