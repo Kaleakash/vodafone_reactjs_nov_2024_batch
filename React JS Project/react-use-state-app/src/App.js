6b20bc003f01732b4ef0907ca13d9071aeffdcbc
Akash <akash300383@gmail.com>
2024-11-12 19:26:12 +0530

    day 2 notes

---
+++ React JS Project/react-use-state-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Sample from './Sample';
+
+function App() {
+  return (
+    <div>
+      <h3>React with useState hook Example</h3>
+      <Sample></Sample>
+    </div>
+  );
+}
+
+export default App;
---
+++ React JS Project/react-use-state-app/src/Sample.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function Sample() {
+let [name,setName]=useState("Raj"); // state variable 
+let age = 21;                       // local variable 
+let handleClick = function() {
+    console.log(name)
+    console.log(age)
+    //setName("Raj Kumar");       // asynchronous 
+    name="Raj Deep"
+    age=24;
+    console.log("After change")
+    console.log(name)
+    console.log(age)
+}
+    return(
+        <div>
+            <h4>Sample Component</h4>
+            <p>Name is {name} Age is {age}</p>
+            <input type="button" value="Change Name" onClick={handleClick}/>
+        </div>
+    )
+
+}
+
+
+export default Sample;
