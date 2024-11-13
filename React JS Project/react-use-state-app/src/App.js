0d0ab25c62741ad7c3d1bb474a51810b6361ccaa
Akash <akash300383@gmail.com>
2024-11-13 17:25:08 +0530

    done

--- React JS Project/react-use-state-app/src/App.js
+++ React JS Project/react-use-state-app/src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Sample from './Sample';
 import Counter from './Couter';
+import Product from './Product';
 
 function App() {
+  // single line comments 
+  /*            
+    Multi line comments 
+  */
   return (
     <div>
       <h3>React with useState hook Example</h3>
-      <Sample></Sample>
+      {/* <Sample></Sample>
       <hr/>
-      <Counter></Counter>
+      <Counter></Counter> */}
+      <Product></Product>
     </div>
   );
 }
---
+++ React JS Project/react-use-state-app/src/Product.js
@@ -0,0 +1,24 @@
+const { useState } = require("react")
+
+function Product() {
+let [product,setProduct]=useState({pid:100,pname:"TV",price:54000});
+
+let changeProduct= function() {
+    //setProduct({pid:101,pname:"Sony Tv",price:56000});// all property change
+    setProduct(previousProduct=>{
+        return {...previousProduct,pid:101}             // ... spread operator part of ES6
+    });
+}
+return(
+        <div>
+            <h2>Product Details</h2>
+            <p>
+                Pid is {product.pid} 
+                PName {product.pname}
+                Price is {product.price}
+            </p>
+    <input type="button" value="Change Product Info" onClick={changeProduct}/>
+        </div>
+    )
+}
+export default Product;
