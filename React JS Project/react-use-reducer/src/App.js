95521c00bbba447d4ee6a938b944c85cd0fa8480
Akash <akash300383@gmail.com>
2024-11-14 19:27:06 +0530

    done

---
+++ React JS Project/react-use-reducer/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>React with userReducer Example</h3>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
---
+++ React JS Project/react-use-reducer/src/Counter.js
@@ -0,0 +1,30 @@
+import { useReducer } from "react";
+
+function reducer(state,action) {
+    //console.log("event fired")
+    console.log(action)
+    console.log(state)
+    if(action.type=="INCREMENT"){
+        return {...state,counter:state.counter+1}
+    }
+    if(action.type=="DECREMENT"){
+        return {...state,counter:state.counter-1}
+    }
+    return state;
+}
+let initialState = {counter:0,name:"Akash"}
+
+function Counter() {    
+let [state,dispatch]=useReducer(reducer,initialState)
+    return(
+        <div>
+            <h3>Trainer name is {state.name}</h3>
+           <p>counter value is {state.counter}</p>
+           <input type="button" value="Increment" onClick={()=>dispatch({type:"INCREMENT"})}/>
+           <input type="button" value="Decrement" onClick={()=>dispatch({type:"DECREMENT"})}/>
+        </div>
+    )
+}
+
+
+export default Counter;
