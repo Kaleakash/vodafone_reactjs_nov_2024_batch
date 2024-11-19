c73c1277722633da4d625051147c39730f898de2
Akash <akash300383@gmail.com>
2024-11-19 17:57:08 +0530

    done

---
+++ React JS Project/react-rest-api/src/Products.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Products() {
+let [products,setProducts]=useState([]);
+
+let loadLiveProductDetails = function() {
+fetch("https://fakestoreapi.com/products").then(response=>response.json()).then(result=>setProducts(result)).catch(error=>console.log(error));
+}
+    return(
+        <div>
+            <h3>All Product Details</h3>
+            <input type="button" value="Load Product"
+            onClick={loadLiveProductDetails}/>
+            {
+                products.map((p,index)=>
+                <div key={index} style={{"display":"inline","width":"400px"}}>
+                    <img src={p.image} width="200px" height="200px"/>
+                    <span>Name : {p.title} Price : {p.price} Description : {p.description}</span>
+                </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default Products;
