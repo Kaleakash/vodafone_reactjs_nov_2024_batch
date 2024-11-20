087c595bb800ed0ceb885b4428fb4d37d70e428c
Akash <akash300383@gmail.com>
2024-11-20 18:53:57 +0530

    done

--- React JS Project/conditional-rendering/src/App.js
+++ React JS Project/conditional-rendering/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
-import React, { createElement } from 'react';
+import React, { createElement, useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  let [flag,setFlag]=useState(true);
+
   //return React.createElement("h1","","Welcome to React JS without JSX")
   //return <h1>Welcome to React JS with JSX</h1>
   
@@ -13,12 +14,13 @@ function App() {
   // const divTag = React.createElement("div","",h1Tag,pTag)
   // return divTag;
 
-  return(
-    <div>
-      <h1>Heading Information</h1>
-      <p className='myClass'>Welcome to REact JS with JSX</p>
-    </div>
-  )
+  // return(
+  //   <div>
+  //     <h1>Heading Information</h1>
+  //     <p className='myClass'>Welcome to REact JS with JSX</p>
+  //   </div>
+  // )
+  return flag ? <h1>Hello</h1> : <h1>Hi</h1>
 }
 
 export default App;
