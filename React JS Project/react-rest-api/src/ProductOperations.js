76cf938faa15e6cce3e6d569f964359edad1f560
Akash <akash300383@gmail.com>
2024-11-20 17:23:09 +0530

    done

--- React JS Project/react-rest-api/src/ProductOperations.js
+++ React JS Project/react-rest-api/src/ProductOperations.js
@@ -2,20 +2,31 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 function ProductOperations() {
 let [products,setProducts]=useState([]);            // array of product 
+
 let [product,setProduct]=useState({})               // only one product 
+
 let [info,setInfo]=useState({});
+
 const URL="http://localhost:3000/products";
+
 useEffect(()=> {
     loadProducts();
 },[info])               // if info value change once again useEffect get called...
+
 let loadProducts=function() {
     //axios.get(URL).then(result=>console.log(result.data)).catch(error=>console.log(error))
     axios.get(URL).then(result=>setProducts(result.data)).catch(error=>console.log(error))
 }
+
 let storeProduct = function(event) {
     event.preventDefault();
     //console.log(product);
     axios.post(URL,product).then(result=>setInfo(result)).catch(error=>console.log(error))
+}
+let handleDelete=function(pid){
+    //console.log(pid);
+    // http://localhost:3000/products/100
+    axios.delete(URL+"/"+pid).then(result=>setInfo(result)).catch(error=>console.log(error))
 }
     return(
         <div>
@@ -38,6 +49,7 @@ let storeProduct = function(event) {
                         <th>PID</th>
                         <th>PName</th>
                         <th>Price</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>    
                 <tbody>
@@ -47,6 +59,7 @@ let storeProduct = function(event) {
                                     <td>{p.id}</td>
                                     <td>{p.pname}</td>
                                     <td>{p.price}</td>
+                                    <td><input type="button" value="delete" onClick={()=>handleDelete(p.id)}/></td>
                                 </tr>
                             )
                         }
