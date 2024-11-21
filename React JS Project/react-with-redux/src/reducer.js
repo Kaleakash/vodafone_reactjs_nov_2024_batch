2e290a6b35fc6e5c9e1d37664e565f3bbd434177
Akash <akash300383@gmail.com>
2024-11-21 17:36:31 +0530

    done

---
+++ React JS Project/react-with-redux/src/reducer.js
@@ -0,0 +1,11 @@
+
+let initialState = {
+    name:"Akash Kale"
+}
+
+let reducer = function(state=initialState,action){
+    // base upon action we can do some changes on state variable. 
+    return state;
+}
+
+export default reducer;
